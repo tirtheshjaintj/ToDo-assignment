0ea3d5f65d03f5d084a3cb27a60edc6c2bf51e14
tj <tirtheshjaintj@gmail.com>
2024-06-24 14:08:10 +0530

    Assignment Done

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeTodo, toggleStatus, updateTodo } from '../features/todo/todoSlice';
 import { motion } from 'framer-motion';
-import { CheckIcon, XMarkIcon, PencilIcon } from '@heroicons/react/24/outline';
 import Modal from 'react-modal';
 import Swal from 'sweetalert2';
 import { Toaster, toast } from 'react-hot-toast';
@@ -102,35 +101,32 @@ function Todos() {
                                     className={`text-white border-0 py-1 px-4 focus:outline-none rounded-md transition-colors ${todo.status === 'completed' ? 'bg-green-500 hover:bg-green-600' : 'bg-gray-500 hover:bg-gray-600'}`}
                                 >
                                     {todo.status === 'completed' ? (
-                                        <CheckIcon className="h-5 w-5" />
+                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+</svg>
                                     ) : (
-                                        <XMarkIcon className="h-5 w-5" />
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+</svg>
                                     )}
                                 </button>
                                 <button
                                     onClick={() => openModal(todo)}
                                     className="ml-2 text-white bg-blue-500 border-0 py-1 px-4 focus:outline-none hover:bg-blue-600 rounded-md"
                                 >
-                                    <PencilIcon className="h-5 w-5" />
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+</svg>
+
                                 </button>
                                 <button
                                     onClick={() => handleDelete(todo.id)}
                                     className="ml-2 text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded-md"
                                 >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1.5}
-                                        stroke="currentColor"
-                                        className="w-6 h-6"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                                        />
-                                    </svg>
+                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+
                                 </button>
                             </div>
                         </div>
@@ -143,9 +139,9 @@ function Todos() {
                 onRequestClose={closeModal}
                 contentLabel="Edit Todo"
                 className="flex items-center justify-center h-full w-[600px]"
-                overlayClassName="bg-black bg-opacity-75 fixed inset-0 flex items-center justify-center"
+                overlayClassName="bg-black bg-opacity-75 fixed inset-0 flex items-center justify-center outline-none" 
             >
-                <div className="bg-gray-900 p-8 rounded-lg shadow-lg w-full max-w-3xl text-white">
+                <div className="bg-gray-900 p-5 rounded-lg shadow-lg w-full text-white">
                     <h2 className="text-2xl font-bold mb-4">Edit Todo</h2>
                     <input
                         type="text"
@@ -163,7 +159,7 @@ function Todos() {
                     <div className="flex justify-end">
                         <button
                             onClick={closeModal}
-                            className="text-gray-400 bg-gray-700 border-0 py-2 px-4 rounded-md mr-2 focus:outline-none hover:bg-gray-600"
+                            className=" bg-gray-700 border-0 py-2 px-4 rounded-md mr-2 focus:outline-none hover:bg-gray-600"
                         >
                             Cancel
                         </button>
@@ -171,7 +167,10 @@ function Todos() {
                             onClick={handleEditSave}
                             className="text-white bg-blue-500 border-0 py-2 px-4 rounded-md focus:outline-none hover:bg-blue-600"
                         >
-                            Save
+                           <div className="flex items-center"> Save <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+</svg></div>
+
                         </button>
                     </div>
                 </div>
